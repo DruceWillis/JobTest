eaba1d5ef271dffe694f6c7a9c34a33964e96a6f
DruceWillis <willis.druce@gmail.com>
2023-05-28 14:07:05 +0300

    assigned materials to vikings, sliced viking UI, added base scripts, added cinemachine and new input system packages

---
+++ JobTest/Assets/Scripts/Configs/BattleEntitiesConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "BattleEntitiesConfig", menuName = "Configs/BattleEntitiesConfig", order = 0)]
+public class BattleEntitiesConfig : ScriptableObject
+{
+    [SerializeField] private List<BattleEntityData> _battleEntities;
+
+    public BattleEntityData GetBattleEntityDataByType(eBattleEntityType type)
+    {
+        return _battleEntities.First(be => be.EntityType == type);
+    }
+}
---
+++ JobTest/Assets/Scripts/Configs/BattleEntityData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "BattleEntityData", menuName = "Configs/BattleEntityData", order = 0)]
+public class BattleEntityData : ScriptableObject
+{
+    [SerializeField] private int _health;
+    [SerializeField] private int _damage;
+    [SerializeField] private eBattleEntityType _entityType;
+
+    public int Health => _health;
+    public int Damage => _damage;
+    public eBattleEntityType EntityType => _entityType;
+}
